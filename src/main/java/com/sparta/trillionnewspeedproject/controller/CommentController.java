7e7d2e57bb59cf6245325fdd38bb5763f4702b98
Haena <johanna0123@naver.com>
2023-07-01 02:10:52 +0900

    선택한 게시글에 대한 모든 댓글 조회 API 추가 및 postid를 Pathvariable로 받는 부분에 대한 코드 수정
    -선택한 게시글 id를 Pathvariable로 받으면 해당 게시글에 대한 댓글을 모두 조회할 수 있도록 추가
    (백엔드만 구현시 필요하지 않지 않을 것 같지만 프론트엔드를 구현시에 필요할 듯 하여 추가함)
    
    -postid를 pathvariable 방식으로 받도록 코드 수정
    -Pathvariable로 받은 postid와 comment의 DB에 존재하는 postid가 일치하지 않다면 예외처리를 하도록 코드 수정
    
    -예외처리에 관한 핸들링이 잘못된 방법인 듯해 해당 부분 이후 수정 예정

--- src/main/java/com/sparta/trillionnewspeedproject/controller/CommentController.java
+++ src/main/java/com/sparta/trillionnewspeedproject/controller/CommentController.java
@@ -11,27 +11,37 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/post")
+@RequestMapping("/api/posts")
 public class CommentController {
 	private final CommentService commentService;
 	private final ResponseMessageUtil responseMessageUtil;
 
+	// 선택한 게시글에 대한 모든 댓글 조회
+	@GetMapping("/{postid}/comments")
+	public List<CommentResponseDto> getCommentsByPostId(@PathVariable Long postid) {
+		return commentService.getCommentsByPostId(postid);
+	}
+
 	// 댓글 작성
-	@PostMapping("/{postid}/comment")
+	@PostMapping("/{postid}/comments")
 	public CommentResponseDto createComment(@PathVariable Long postid, @RequestBody CommentRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
 		return commentService.createComment(postid, requestDto, userDetails.getUser());
 	}
 
 	// 선택한 댓글 수정
-	@PutMapping("/{postid}/comment/{commentid}")
+	@PutMapping("/{postid}/comments/{commentid}")
 	public CommentResponseDto updateComment(@PathVariable Long postid, @PathVariable Long commentid, @RequestBody CommentRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails, HttpServletResponse response) throws IOException {
 		CommentResponseDto responseDto = commentService.updateComment(postid, commentid, requestDto, userDetails.getUser(), response);
 
 		if (response.getStatus() == 400) {
 			responseMessageUtil.statusResponse(response, "작성자만 수정할 수 있습니다.");
+			return null;
+		} else if (response.getStatus() == 404) {
+			responseMessageUtil.statusResponse(response, "주소가 잘못되어 해당 댓글을 찾을 수 없습니다.");
 			return null;
 		} else {
 			return responseDto;
@@ -39,22 +49,31 @@ public class CommentController {
 	}
 
 	// 선택한 댓글 삭제
-	@DeleteMapping("/{postid}/comment/{commentid}")
+	@DeleteMapping("/{postid}/comments/{commentid}")
 	public void deleteComment(@PathVariable Long postid, @PathVariable Long commentid, @AuthenticationPrincipal UserDetailsImpl userDetails, HttpServletResponse response) throws IOException {
 		commentService.deleteComment(postid, commentid, userDetails.getUser(), response);
 
 		if (response.getStatus() == 400) {
 			responseMessageUtil.statusResponse(response, "작성자만 삭제할 수 있습니다.");
+		} else if (response.getStatus() == 404) {
+			responseMessageUtil.statusResponse(response, "주소가 잘못되어 해당 댓글을 찾을 수 없습니다.");
 		} else {
 			responseMessageUtil.statusResponse(response, "해당 댓글의 삭제를 완료하였습니다.");
 		}
 	}
 
 	// 선택한 댓글 좋아요
-	@PutMapping("/comments/{id}/likes")
-	public CommentResponseDto commentLike(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails, HttpServletResponse response) throws IOException {
-		CommentResponseDto responseDto = commentService.commentLike(id, userDetails.getUser(), response);
-
-		return responseDto;
+	@PutMapping("/{postid}/comments/{commentid}/likes")
+	public CommentResponseDto commentLike(@PathVariable Long postid, @PathVariable Long commentid, @AuthenticationPrincipal UserDetailsImpl userDetails, HttpServletResponse response) throws IOException {
+		if (response.getStatus() == 400) {
+			responseMessageUtil.statusResponse(response, "본인이 작성한 댓글에는 좋아요를 누를 수 없습니다.");
+			return null;
+		} else if (response.getStatus() == 404) {
+			responseMessageUtil.statusResponse(response, "주소가 잘못되어 해당 댓글을 찾을 수 없습니다.");
+			return null;
+		} else {
+			CommentResponseDto responseDto = commentService.commentLike(postid, commentid, userDetails.getUser(), response);
+			return responseDto;
+		}
 	}
 }
--- src/main/java/com/sparta/trillionnewspeedproject/service/CommentService.java
+++ src/main/java/com/sparta/trillionnewspeedproject/service/CommentService.java
@@ -22,6 +22,11 @@ public class CommentService {
 	private final CommentRepository commentRepository;
 	private final PostRepository postRepository;
 
+	// 선택한 게시글에 대한 댓글 전체 조회
+	public List<CommentResponseDto> getCommentsByPostId(Long postid) {
+		return commentRepository.findAllByPost_idOrderByCreatedAtDesc(postid).stream().map(CommentResponseDto::new).toList();
+	}
+
 	// 댓글 작성
 	public CommentResponseDto createComment(Long postid, CommentRequestDto requestDto, User user) {
 		Post post = postRepository.getById(postid);
@@ -34,37 +39,40 @@ public class CommentService {
 	// 선택한 댓글 수정
 	@Transactional
 	public CommentResponseDto updateComment(Long postid, Long commentid, CommentRequestDto requestDto, User user, HttpServletResponse response) {
-		if (!checkUser(commentid, user)) {
+		if (postid != findComment(commentid).getPost().getId()) {
+			response.setStatus(404);
+			return null;
+		} else if (!checkUser(commentid, user)) {
 			response.setStatus(400);
 			return null;
 		} else {
 			findComment(commentid).update(requestDto);
-			CommentResponseDto commentResponseDto = new CommentResponseDto(findComment(id));
+			CommentResponseDto commentResponseDto = new CommentResponseDto(findComment(commentid));
 			return commentResponseDto;
 		}
 	}
 
 	// 선택한 댓글 삭제
 	public void deleteComment(Long postid, Long commentid, @AuthenticationPrincipal User user, HttpServletResponse response) {
-		if (!checkUser(commentid, user)) {
+		if (postid != findComment(commentid).getPost().getId()) {
+			response.setStatus(404);
+		} else if (!checkUser(commentid, user)) {
 			response.setStatus(400);
 		} else {
 			commentRepository.delete(findComment(commentid));
 		}
 	}
 
-	// 선택한 게시글에 대한 댓글 전체 조회
-	public List<CommentResponseDto> getComments(Long postid) {
-		return commentRepository.findAllByPost_idOrderByCreatedAtDesc(postid).stream().map(CommentResponseDto::new).toList();
-	}
-
 	// 선택한 댓글 좋아요 기능 추가
-	public CommentResponseDto commentLike(Long id, User user, HttpServletResponse response) {
-		if (checkUser(id, user)) {
+	public CommentResponseDto commentLike(Long postid, Long commentid, User user, HttpServletResponse response) {
+		if (postid != findComment(commentid).getPost().getId()) {
+			response.setStatus(404);
+			return null;
+		} else if (checkUser(commentid, user)) {
 			response.setStatus(400);
 			return null;
 		} else {
-			Comment comment = findComment(id);
+			Comment comment = findComment(commentid);
 			comment.updateLikes();
 			Comment savedComment = commentRepository.save(comment);
 			CommentResponseDto commentResponseDto = new CommentResponseDto(savedComment);
@@ -73,8 +81,15 @@ public class CommentService {
 	}
 
 	// id에 따른 댓글 찾기
-	private Comment findComment(Long id) {
-		return commentRepository.findById(id).orElseThrow(() ->
+	private Comment findComment(Long commentid) {
+		return commentRepository.findById(commentid).orElseThrow(() ->
+				new IllegalArgumentException("선택한 게시글은 존재하지 않습니다.")
+		);
+	}
+
+	// id에 따른 게시글 찾기
+	private Post findPost(Long id) {
+		return postRepository.findById(id).orElseThrow(() ->
 				new IllegalArgumentException("선택한 게시글은 존재하지 않습니다.")
 		);
 	}
