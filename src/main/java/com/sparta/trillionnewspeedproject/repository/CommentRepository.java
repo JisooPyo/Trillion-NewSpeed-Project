2b874057ef025ee8ff3d0157e6d04e78997cf91b
JisooPyo <130378232+JisooPyo@users.noreply.github.com>
2023-07-05 15:24:42 +0900

    Delete CommentRepository.java

--- src/main/java/com/sparta/trillionnewspeedproject/repository/CommentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sparta.trillionnewspeedproject.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sparta.trillionnewspeedproject.entity.Comment;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
