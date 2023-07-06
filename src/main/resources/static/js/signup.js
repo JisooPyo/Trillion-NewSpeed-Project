453e53d6d442e8b8a3d77390f6dbdf7f4a38ab2c
JisooPyo <audwl326@gmail.com>
2023-07-06 23:25:55 +0900

    이메일 보내서 인증번호 받기 성공.

--- src/main/resources/static/js/signup.js
+++ src/main/resources/static/js/signup.js
@@ -40,3 +40,35 @@ function showCheckPassword() {
 function home() {
   window.location.href = "../templates/newspeed.html";
 }
+
+var password1 = document.getElementById("password");
+var password2 = document.getElementById("check-password");
+var comparePassword = document.getElementById("compare-password");
+
+password1.addEventListener("input", compareInputs);
+password2.addEventListener("input", compareInputs);
+
+function compareInputs() {
+  var val1 = password1.value;
+  var val2 = password2.value;
+
+  if (val1 === val2) {
+    comparePassword.textContent = "비밀번호가 일치합니다.";
+  } else {
+    comparePassword.textContent = "비밀번호가 일치하지 않습니다.";
+  }
+}
+
+$(document).ready(function () {
+  $("#email-check-button").click(function () {
+    var email = $("#email").val(); // 입력된 이메일 가져오기
+
+    // AJAX를 통해 POST 요청 보내기
+    $.ajax({
+      url: "/api/user/signup/authentication", // API 엔드포인트 URL
+      type: "POST",
+      data: JSON.stringify({ email: email }), // 요청 데이터 설정
+      contentType: "application/json", // 요청의 컨텐츠 타입 설정
+    });
+  });
+});
